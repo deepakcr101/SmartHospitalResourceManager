344f7c2b549aa5f7314c6d028f5957941ed15c17
deepak <dkumar6@ciena.com>
2026-02-02 10:20:42 +0530

    Initial Project Setup

---
+++ src/main/java/com/hospital/resource_manager/SmartHospitalResourceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.resource_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHospitalResourceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHospitalResourceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/resource_manager/SmartHospitalResourceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.resource_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHospitalResourceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
